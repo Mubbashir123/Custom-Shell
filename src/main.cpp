9548c978d20207d0b62e414c6ff017ab839990b3
Mohd Mubbashir <www.mohdmubbashir@gmail.com>
2025-12-12 18:41:55 +0530

    printing command

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+ 
+  string command;
+  getline(cin,command);
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  cout<<command<<": command not found"<<endl;
 }
