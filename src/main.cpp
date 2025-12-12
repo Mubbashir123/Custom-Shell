5f7f816151c8d9161403335c9279658f60b47549
Mohd Mubbashir <www.mohdmubbashir@gmail.com>
2025-12-12 18:43:41 +0530

    Fix test failure

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <string>
 using namespace std;
 int main() {
- 
+  cout<<"$ ";
   string command;
   getline(cin,command);
-
+  if(!command.empty())
   cout<<command<<": command not found"<<endl;
 }
