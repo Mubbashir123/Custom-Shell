ff281143d98307e61ddd102346460fbc76df1fdc
Mohd Mubbashir <www.mohdmubbashir@gmail.com>
2025-12-13 06:47:54 +0530

    Builtin

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,18 @@ int main() {
     string command;
   getline(cin,command);
   if(command=="exit") break;
-   
+  else if (command.find("type")!=string::npos)
+  {
+    if(command.substr(5)=="echo"|| command.substr(5)=="exit"|| command.substr(5)=="type")
+    {
+      cout<<command.substr(5)<<" is a shell builtin"<<endl;
+    }
+    else cout<<command.substr(5)<<": command not found"<<endl;
+  }
   else if(command.find("echo")!=string::npos)
   {
     cout<<command.substr(5)<<endl;
   }
-  
   else
   cout<<command<<": command not found"<<endl;
 }
