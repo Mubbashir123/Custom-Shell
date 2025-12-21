804cea53d5ee2e2e75e2264306c76a8dee965563
Mohd Mubbashir <www.mohdmubbashir@gmail.com>
2025-12-21 23:35:23 +0530

    locate exec

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,67 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+#include <unistd.h>
 using namespace std;
-int main() {
-  while(true)
-  { 
-    cout<<"$ ";
+int main()
+{
+  while (true)
+  {
+    cout << "$ ";
     string command;
-  getline(cin,command);
-  if(command=="exit") break;
-  else if (command.find("type")!=string::npos)
-  {
-    if(command.substr(5)=="echo"|| command.substr(5)=="exit"|| command.substr(5)=="type")
+    getline(cin, command);
+    if (command == "exit")
+      break;
+    else if (command.find("type") != string::npos)
     {
-      cout<<command.substr(5)<<" is a shell builtin"<<endl;
+      if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type")
+      {
+        cout << command.substr(5) << " is a shell builtin" << endl;
+      }
+      else
+      {
+        string cmd = command.substr(5);
+        char *pathenv = getenv("PATH");
+        if (pathenv == NULL)
+        {
+          cout << cmd << ": not found " << endl;
+          continue;
+        }
+        string path(pathenv);
+        bool f = false;
+
+        char sep = ';'; // seperator for windows
+        int st = 0;
+        for (int i = 0; i <= path.size(); i++)
+        {
+          if (i == path.size() || path[i] == sep)
+          {
+            string dir = path.substr(st, i - st);
+            st = i + 1;
+
+            if (dir.empty())
+              continue;
+
+            string fullPath = dir + "/" + cmd;
+
+            if (access(fullPath.c_str(), X_OK) == 0)
+            {
+              cout << cmd << " is " << fullPath << endl;
+              f = true;
+              break;
+            }
+          }
+        }
+
+        if (!f)
+        {
+          cout << cmd << ": not found" << endl;
+        }
+      }
     }
-    else cout<<command.substr(5)<<": not found"<<endl;
+    else if (command.find("echo") != string::npos)
+    {
+      cout << command.substr(5) << endl;
+    }
+    else
+      cout << command << ": command not found" << endl;
   }
-  else if(command.find("echo")!=string::npos)
-  {
-    cout<<command.substr(5)<<endl;
-  }
-  else
-  cout<<command<<": command not found"<<endl;
-}
-
-
 }
