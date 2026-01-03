33d94ee07a57ca12dafdf48d4f259e7409a2f5e3
Mohd Mubbashir <www.mohdmubbashir@gmail.com>
2026-01-03 23:23:46 +0530

    Added redirection

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,39 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 using namespace std;
+void runCommand(vector<string>args)
+{
+    auto it=find(args.begin(),args.end(),">");
+    if(it!=args.end())
+    {
+        int index=distance(args.begin(),it);
+        if(index+1<args.size())
+        {
+            string filename=args[index+1];
+            int fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            
+            if(fd<0)
+            {
+                perror("Failed to open file");
+                exit(1);
+            }
+            dup2(fd,STDOUT_FILENO);
+            close(fd);
+
+            args.erase(it,it+2);
+
+        }
+        
+    }
+    vector<char *> c_args;
+    for (auto &a : args)
+        c_args.push_back(&a[0]);
+    c_args.push_back(NULL);
+    execvp(c_args[0], c_args.data());
+
+    perror("Exec failed");
+    exit(1);
+}
 vector<string> parseArgs(const string &line)
 {
     vector<string> args;
@@ -50,44 +83,50 @@ int main()
         string command;
         // getline(cin, command);
 
-        if(!getline(cin,command)) break;
+        if (!getline(cin, command))
+            break;
 
-        if(command.empty()) continue;
+        if (command.empty())
+            continue;
 
         auto args = parseArgs(command);
-        if (args.empty()) continue;
+        if (args.empty())
+            continue;
 
-        if (args[0] == "exit") break;
-        
-        else if (args[0] == "cd") {
-             if (args.size() < 2) {
-                 chdir(getenv("HOME"));
-             } else {
-                 if (chdir(args[1].c_str()) != 0) {
-                     perror("cd failed");
-                 }
-             }
-             continue;
+        if (args[0] == "exit")
+            break;
+
+        else if (args[0] == "cd")
+        {
+            if (args.size() < 2)
+            {
+                chdir(getenv("HOME"));
+            }
+            else
+            {
+                if (chdir(args[1].c_str()) != 0)
+                {
+                    perror("cd failed");
+                }
+            }
+            continue;
         }
 
-        else {
-            pid_t pid = fork(); 
-            if (pid < 0) {
+        else
+        {
+            pid_t pid = fork();
+            if (pid < 0)
+            {
                 perror("Fork failed");
-            } 
-            else if (pid == 0) {
-                
-                vector<char*> c_args;
-                for (auto &a : args) c_args.push_back(&a[0]);
-                c_args.push_back(NULL); 
-                execvp(c_args[0], c_args.data());
-                
-                perror("Exec failed"); 
-                exit(1); 
-            } 
-            else {
-                int status;
-                waitpid(pid, &status, 0);
+            }
+            else if (pid == 0)
+            {
+
+                runCommand(args);
+            }
+            else
+            {
+                wait(NULL);
             }
         }
     }
